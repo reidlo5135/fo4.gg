46e8a75f12928de70a85803a6d09bc52dd51120d
reidlo5135 <201840103@email.daelim.ac.kr>
2022-07-29 19:07:39 +0900

    07/29 19:07 REFACTOR: Logger 추가

--- src/main/java/com/reidlo/fo4/home/HomeController.java
+++ src/main/java/com/reidlo/fo4/home/HomeController.java
@@ -1,16 +1,16 @@
 package com.reidlo.fo4.home;
 
-import lombok.extern.slf4j.Slf4j;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Slf4j
 @Controller
 public class HomeController {
+    private final Logger log = Logger.getLogger(getClass());
 
     @GetMapping(value = "/")
     public String home() {
-        System.out.println("Home Controller");
+        log.info("HOME!");
         return "index";
     }
 }
