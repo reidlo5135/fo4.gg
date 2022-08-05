587b5648a5c2e5aba5739f0fa592b1243fd06b40
reidlo5135 <201840103@email.daelim.ac.kr>
2022-08-05 21:39:35 +0900

    08/05 21:38 REFACTOR: MainController.java 수정

--- src/main/java/com/reidlo/fo4/main/controller/MainController.java
+++ src/main/java/com/reidlo/fo4/main/controller/MainController.java
@@ -3,6 +3,7 @@ package com.reidlo.fo4.main.controller;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MainController {
@@ -12,5 +13,19 @@ public class MainController {
     public String home() {
         log.info("MainController homepage");
         return "index";
+    }
+
+    @GetMapping(value = "/details/pvp/{nickname}/{level}/{pvpName}/{pvpDate}")
+    public String pvpDetails(@PathVariable String nickname, @PathVariable String level, @PathVariable String pvpName, @PathVariable String pvpDate) {
+        log.info("move to pvpDetails");
+        log.info("Details pvpPath : " + nickname + ", " + level + ", " + pvpName + ", " + pvpDate);
+        return "/match/pvpDetails";
+    }
+
+    @GetMapping(value = "/details/coach/{nickname}/{level}/{coachName}/{coachDate}")
+    public String coachDetails(@PathVariable String nickname, @PathVariable String level, @PathVariable String coachName, @PathVariable String coachDate) {
+        log.info("move to coachDetails");
+        log.info("Details coachPath : " + nickname + ", " + level + ", " + coachName + ", " + coachDate);
+        return "/match/coachDetails";
     }
 }
