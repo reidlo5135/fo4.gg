4cb3b2788eecfcf50d26b14374766cc94219f945
reidlo5135 <201840103@email.daelim.ac.kr>
2022-08-06 11:52:00 +0900

    08/06 11:51 REFACTOR: UserInfoRepository.java logic 수정

--- src/main/java/com/reidlo/fo4/userInfo/model/UserInfoRepository.java
+++ src/main/java/com/reidlo/fo4/userInfo/model/UserInfoRepository.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Repository;
 
+import java.sql.SQLException;
 import java.util.List;
 
 @Repository
@@ -13,19 +14,19 @@ public class UserInfoRepository {
     private final Logger log = Logger.getLogger(getClass());
     private final UserInfoMapper userInfoMapper;
 
-    public List<User> findAll() {
+    public List<User> findAll() throws SQLException {
         List<User> list = userInfoMapper.findAll();
         log.info("UserInfoRepo findAll list : " + list);
         return list;
     }
 
-    public List<User> findByNickName(String nickname) {
+    public List<User> findByNickName(String nickname) throws SQLException {
         log.info("UserInfoRepo findByNickName nickname : " + nickname);
         List<User> list = userInfoMapper.findByNickName(nickname);
         return list;
     }
 
-    public int register(User user) {
+    public int register(User user) throws SQLException {
         log.info("UserInfoRepo register user : " + user);
         int result = userInfoMapper.register(user);
         log.info("UserInfoRepo register result : " + result);
