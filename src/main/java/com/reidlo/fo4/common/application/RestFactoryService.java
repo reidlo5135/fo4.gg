2cab24cb5c2c13c3a56a5c5b314415916ed55d84
reidlo5135 <201840103@email.daelim.ac.kr>
2022-10-25 10:37:10 +0900

    REFACTOR: Modify RestFactoryService.java

--- src/main/java/com/reidlo/fo4/common/application/RestFactoryService.java
+++ src/main/java/com/reidlo/fo4/common/application/RestFactoryService.java
@@ -15,17 +15,17 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class RestFactoryService {
     private static final String API_KEY = RestFactory.API_KEY;
-    private static final String UserInfoUrl = RestFactory.UserInfoUrl;
-    private static final String UserMaxDivisionUrl = RestFactory.UserMaxDivisionUrl;
-    private static final String DivisionJSONUrl = RestFactory.DivisionJSONUrl;
-    private static String DefaultDivisionImageUrl = RestFactory.DefaultDivisionImageUrl;
+    private static final String USER_INFO_URL = RestFactory.USER_INFO_URL;
+    private static final String USER_MAX_DIVISION_URL = RestFactory.USER_MAX_DIVISION_URL;
+    private static final String DIVISION_JSON_URL = RestFactory.DIVISION_JSON_URL;
+    private static final String DEFAULT_DIVISION_IMAGE_URL = RestFactory.DEFAULT_DIVISION_IMAGE_URL;
 
     public String requestUserInfoByNickName(String nickname) {
         final HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.set("Authorization", API_KEY);
 
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.exchange(UserInfoUrl, HttpMethod.GET, new HttpEntity<>(httpHeaders), String.class, nickname).getBody();
+        return restTemplate.exchange(USER_INFO_URL, HttpMethod.GET, new HttpEntity<>(httpHeaders), String.class, nickname).getBody();
     }
 
     public List<Map<String, Object>> requestUserMaxDivisionByAccessId(String accessId) {
@@ -33,7 +33,7 @@ public class RestFactoryService {
         httpHeaders.set("Authorization", API_KEY);
 
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.exchange(UserMaxDivisionUrl, HttpMethod.GET, new HttpEntity<>(httpHeaders), List.class, accessId).getBody();
+        return restTemplate.exchange(USER_MAX_DIVISION_URL, HttpMethod.GET, new HttpEntity<>(httpHeaders), List.class, accessId).getBody();
     }
 
     public List<Map<String, Object>> requestDivisionJSON() {
@@ -41,38 +41,38 @@ public class RestFactoryService {
         httpHeaders.set("Authorization", API_KEY);
 
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.exchange(DivisionJSONUrl, HttpMethod.GET, new HttpEntity<>(httpHeaders), List.class).getBody();
+        return restTemplate.exchange(DIVISION_JSON_URL, HttpMethod.GET, new HttpEntity<>(httpHeaders), List.class).getBody();
     }
 
     public String setDivisionImageUrl(String division) {
         String DivisionImageUrl = null;
         switch (division) {
             case "월드클래스1":
-                DivisionImageUrl = DefaultDivisionImageUrl + 6 + ".png";
+                DivisionImageUrl = DEFAULT_DIVISION_IMAGE_URL + 6 + ".png";
                 break;
             case "월드클래스2":
-                DivisionImageUrl = DefaultDivisionImageUrl + 7 + ".png";
+                DivisionImageUrl = DEFAULT_DIVISION_IMAGE_URL + 7 + ".png";
                 break;
             case "월드클래스3":
-                DivisionImageUrl = DefaultDivisionImageUrl + 8 + ".png";
+                DivisionImageUrl = DEFAULT_DIVISION_IMAGE_URL + 8 + ".png";
                 break;
             case "프로1":
-                DivisionImageUrl = DefaultDivisionImageUrl + 9 + ".png";
+                DivisionImageUrl = DEFAULT_DIVISION_IMAGE_URL + 9 + ".png";
                 break;
             case "프로2":
-                DivisionImageUrl = DefaultDivisionImageUrl + 10 + ".png";
+                DivisionImageUrl = DEFAULT_DIVISION_IMAGE_URL + 10 + ".png";
                 break;
             case "프로3":
-                DivisionImageUrl = DefaultDivisionImageUrl + 11 + ".png";
+                DivisionImageUrl = DEFAULT_DIVISION_IMAGE_URL + 11 + ".png";
                 break;
             case "세미프로1":
-                DivisionImageUrl = DefaultDivisionImageUrl + 12 + ".png";
+                DivisionImageUrl = DEFAULT_DIVISION_IMAGE_URL + 12 + ".png";
                 break;
             case "세미프로2":
-                DivisionImageUrl = DefaultDivisionImageUrl + 13 + ".png";
+                DivisionImageUrl = DEFAULT_DIVISION_IMAGE_URL + 13 + ".png";
                 break;
             case "세미프로3":
-                DivisionImageUrl = DefaultDivisionImageUrl + 14 + ".png";
+                DivisionImageUrl = DEFAULT_DIVISION_IMAGE_URL + 14 + ".png";
                 break;
             case "기록이 존재하지 않습니다.":
                 break;
